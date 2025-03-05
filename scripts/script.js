f072812f8ba92f011dca105602affa8463d41a4a
wandereryiu <yiyzhao1@gmail.com>
2025-03-04 23:01:05 -0800

    Add Firebase features to main.html

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,29 @@ function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
+}
+
+// Function to get the current user's display name
+function displayUserName() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            document.getElementById('userName').innerHTML = "Hello, " + user.displayName + "!";
+        } else {
+            document.getElementById('userName').innerHTML = "Hello, Guest!";
+        }
+    });
+}
+
+// Call the function when the page loads
+displayUserName();
